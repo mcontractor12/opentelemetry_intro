eb58abd80249fba9534c895d43a566f00030b00f
Maheen Contractor <mcontractor@microsoft.com>
2023-08-03 13:44:18 -0400

    updates to debug files

---
+++ Silo/Configuration/AppExporters.cs
@@ -0,0 +1,9 @@
+﻿namespace Silo.Configuration
+{
+    public class AppExporters
+    {
+        public string AppInisghtsConnectionString { get; set; } = string.Empty;
+
+        public string GenevaConnectionString { get; set; } = string.Empty;
+    }
+}
--- Silo/obj/Debug/net6.0/Silo.AssemblyInfo.cs
+++ Silo/obj/Debug/net6.0/Silo.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("ec0d10cd-bbad-4261-8c96-d7625a0b05d7")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Silo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
